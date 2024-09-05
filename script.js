acc55abf47d40218c445659ade9cd55b570007d9
NIMMY04 <156294603+NIMMY04@users.noreply.github.com>
2024-09-05 21:21:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  let sum=0;
+	const lists=new Array();
+	let res=new Array();
+	arr.forEach(a=>
+		{
+			if(sum+a<=n)
+			{
+				res.push(a);
+				sum+=a;
+			}
+			else
+			{
+				lists.push(res);
+				res=new Array();
+				res.push(a);
+				sum=a;
+				
+			}
+		});
+	lists.push(res);
+	return lists;
+};
+
+const n =prompt("Enter n: ");
+alert(JSON.stringify(divide(arr, n)));
 };
 
 const n = prompt("Enter n: ");
